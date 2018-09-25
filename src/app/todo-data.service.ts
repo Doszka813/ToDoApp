c6003f1e5ec552144de815fed4409a7ec321f4b1
Dominika Kaminska <lewillas@gmail.com>
2018-09-25 11:30:33 +0200

    initial commit

---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  lastId = 0;
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  addTodo (todo: Todo) {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  deleteTodoById(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+    return this;
+  }
+
+  updateTodoById(id: number, values: Object = {}) {
+    // tslint:disable-next-line:prefer-const
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  getTodoById(id: number): Todo {
+    return this.todos.filter(todo => todo.id === id).pop();
+  }
+
+  toggleTodoComplete(todo: Todo) {
+    const updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
+}
